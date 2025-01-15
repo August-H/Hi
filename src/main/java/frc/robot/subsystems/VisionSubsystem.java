c299bf1e6e362199494b60472d1eecc09f3262eb
AquiSyl25 <104875457+AquiSyl25@users.noreply.github.com>
2025-01-14 16:32:44 -0800

    Update VisionSubsystem.java

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -36,9 +36,9 @@ public class VisionSubsystem extends SubsystemBase { // handles LL3 April Tag De
   private Vector m_targetVector; // holds vector from target to camera
 
   public VisionSubsystem() {
-    m_NetworkTableFront = NetworkTableInstance.getDefault().getTable("photonvision").getSubTable("Camera1OV9281");
-    // m_NetworkTableBack = NetworkTableInstance.getDefault().getTable("limelight-back");
-    m_targetVector = VecBuilder.fill(20, 20, 20); // arbitrary big values -- if the robot starts out not seeing april
+    // m_NetworkTableFront = NetworkTableInstance.getDefault().getTable("photonvision").getSubTable("Camera1OV9281");
+    // // m_NetworkTableBack = NetworkTableInstance.getDefault().getTable("limelight-back");
+    // m_targetVector = VecBuilder.fill(20, 20, 20); // arbitrary big values -- if the robot starts out not seeing april
                                                   // tag, it does not trust from the get go
   }
 
@@ -48,41 +48,41 @@ public class VisionSubsystem extends SubsystemBase { // handles LL3 April Tag De
 
   
 
-  public Optional<EstimatedRobotPose> aprilTagUpdate() // updates estimated robot pose based on april tags seen
-  {
-    double rawBotPose[];
-    Boolean validTargetsPresent = m_NetworkTableFront.getEntry("hasTarget").getBoolean(false);
-    // SmartDashboard.putBoolean("valid targets", validTargetsPresent);
+  // public Optional<EstimatedRobotPose> aprilTagUpdate() // updates estimated robot pose based on april tags seen
+  // {
+  //   double rawBotPose[];
+  //   Boolean validTargetsPresent = m_NetworkTableFront.getEntry("hasTarget").getBoolean(false);
+  //   // SmartDashboard.putBoolean("valid targets", validTargetsPresent);
 
-    if (validTargetsPresent) // returns bot pose if april tags are seen
-    {
-      // gets blue pose (assuming a single coordinate system for both alliances)
-      rawBotPose = m_NetworkTableFront.getEntry("targetPose").getDoubleArray(new double[7]);
-      // SmartDashboard.putNumber("pose x", rawBotPose[0]);
-      // SmartDashboard.putNumber("pose y", rawBotPose [1]);
-      // SmartDashboard.putNumber("pose z", rawBotPose [2]);
+  //   if (validTargetsPresent) // returns bot pose if april tags are seen
+  //   {
+  //     // gets blue pose (assuming a single coordinate system for both alliances)
+  //     rawBotPose = m_NetworkTableFront.getEntry("targetPose").getDoubleArray(new double[7]);
+  //     // SmartDashboard.putNumber("pose x", rawBotPose[0]);
+  //     // SmartDashboard.putNumber("pose y", rawBotPose [1]);
+  //     // SmartDashboard.putNumber("pose z", rawBotPose [2]);
 
-      double[] targetArr = m_NetworkTableFront.getEntry("targetPose").getDoubleArray(new double[7]); // gets
-                                                                                                                 // vector
-                                                                                                                 // from
-                                                                                                                 // target
-                                                                                                                 // to
-                                                                                                                 // camera
-      m_targetVector = VecBuilder.fill(targetArr[0], targetArr[1], targetArr[2]); // passes x, y, z values into 3d
-                                                                                  // vector (so we can get l3 norm
-                                                                                  // instead of l7)
+  //     double[] targetArr = m_NetworkTableFront.getEntry("targetPose").getDoubleArray(new double[7]); // gets
+  //                                                                                                                // vector
+  //                                                                                                                // from
+  //                                                                                                                // target
+  //                                                                                                                // to
+  //                                                                                                                // camera
+  //     m_targetVector = VecBuilder.fill(targetArr[0], targetArr[1], targetArr[2]); // passes x, y, z values into 3d
+  //                                                                                 // vector (so we can get l3 norm
+  //                                                                                 // instead of l7)
 
-      return Optional.of(new EstimatedRobotPose(
-          new Pose3d(new Translation3d(rawBotPose[0], rawBotPose[1], rawBotPose[2]),
-              new Rotation3d(Math.toRadians(rawBotPose[3]),
-                  Math.toRadians(rawBotPose[4]), Math.toRadians(rawBotPose[5])))
-              .toPose2d(),
-          Timer.getFPGATimestamp() - (rawBotPose[6] / 1000.0),
-          m_targetVector)); // returns pose based on april tag
-    } else {
-      return Optional.empty(); // returns an empty if not seeing april tags
-    }
-  }
+  //     return Optional.of(new EstimatedRobotPose(
+  //         new Pose3d(new Translation3d(rawBotPose[0], rawBotPose[1], rawBotPose[2]),
+  //             new Rotation3d(Math.toRadians(rawBotPose[3]),
+  //                 Math.toRadians(rawBotPose[4]), Math.toRadians(rawBotPose[5])))
+  //             .toPose2d(),
+  //         Timer.getFPGATimestamp() - (rawBotPose[6] / 1000.0),
+  //         m_targetVector)); // returns pose based on april tag
+  //   } else {
+  //     return Optional.empty(); // returns an empty if not seeing april tags
+  //   }
+  // }
 
   public boolean seesTargets() // returns whether robot sees april tags (used for triggers)
   {
